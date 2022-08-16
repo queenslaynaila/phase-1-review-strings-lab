5bde6093d3c143a2b3ef1e5ddbc96f13e9729e60
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-16 19:28:04 +0300

    Completed ASsignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Queenslay Jema";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage =
+  "WELCOME TO FLATBOOK, " + "QUEENSLAY JEMA" + "!";
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
